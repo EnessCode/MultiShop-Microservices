eca12a46dc600777cc68c1a750016085816be2eb
Enes <onerenes12@gmail.com>
2026-06-06 01:37:37 +0300

    feat(basket): add basket management with Redis cache and identity protection
    - Integrated Redis to store and manage user basket data in-memory for fast performance.
    - Created LoginService to automatically get the logged-in user's ID from JWT tokens.
    - Implemented BasketService with full async methods to get, save, and delete baskets.
    - Added a global AuthorizeFilter to secure all basket endpoints against unauthorized access.
    - Developed BasketsController to expose clean RESTful endpoints for the basket UI.

---
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
+using System.Threading.Tasks;
+
+namespace MultiShop.Basket.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BasketsController : ControllerBase
+	{
+		private readonly IBasketService _basketService;
+		private readonly ILoginService _loginService;
+
+		public BasketsController(IBasketService basketService, ILoginService loginService)
+		{
+			_basketService = basketService;
+			_loginService = loginService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetMyBasketDetail()
+		{
+			var user = User.Claims;
+			var userId = _loginService.GetUserId;
+			var values = await _basketService.GetBasket(userId);
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto)
+		{
+			basketTotalDto.UserId = _loginService.GetUserId;
+			await _basketService.SaveBasket(basketTotalDto);
+			return Ok("Sepetteki değişiklikler başarıyla kaydedildi.");
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteBasket()
+		{
+			var userId = _loginService.GetUserId;
+			await _basketService.DeleteBasket(userId);
+			return Ok("Sepet başarıyla silindi.");
+		}
+	}
+}
---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+	public class LoginService : ILoginService
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public LoginService(IHttpContextAccessor contextAccessor)
+		{
+			_httpContextAccessor = contextAccessor;
+		}
+
+		public string GetUserId =>
+					_httpContextAccessor.HttpContext?.User?.FindFirst("sub")?.Value
+					?? _httpContextAccessor.HttpContext?.User?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+	}
+}
